010fb6e802243811d37bfa7aed4688a379021438
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2024-11-29 07:01:04 +0000

    update: connect to redis instance, set an item with TTL, and retrieve item

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+
+IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true);
+IConfigurationRoot root = builder.Build();
+
+string? redisConnectionString = root.GetValue<string>("ConnectionStrings:CacheForRedis");
+
+if (string.IsNullOrEmpty(redisConnectionString))
+{
+    throw new ArgumentNullException(nameof(redisConnectionString));
+}
+
+void SetItem(IDatabase database, string key) {
+    var product = database.StringGet(key);
+
+    Console.WriteLine($"Product: {product}");
+
+    if (!product.HasValue)
+    {
+        database.StringSet(key, "New Product", TimeSpan.FromSeconds(60));
+
+        Console.WriteLine("Setting product value...");
+    }
+
+    product = database.StringGet(key);
+
+    Console.WriteLine($"Product: {product}");
+}
+
+using (var redis = ConnectionMultiplexer.Connect(redisConnectionString))
+{
+    var database = redis.GetDatabase();
+
+    string key = "product:123";
+
+    SetItem(database, key);
+}
